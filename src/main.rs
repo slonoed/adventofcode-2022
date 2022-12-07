df6e70a0c2b0c0086e137b1ae02bbf99d453c195
Dmitry Manannikov <email@slonoed.net>
2022-12-06 17:59:59 -0800

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+const PATH: &str = "data/day-1.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open(PATH).expect("cant open file");
+    let reader = BufReader::new(file);
+
+    let mut max: i32 = 0;
+    let mut acc: i32 = 0;
+
+    for line in reader.lines() {
+        let text = line.expect("reading line");
+        if text == "" {
+            if acc > max {
+                max = acc;
+            }
+
+            acc = 0;
+        }
+
+        let num: i32 = text.parse().unwrap_or_default();
+        acc = acc + num;
+    }
+
+    print!("{}", max);
 }
